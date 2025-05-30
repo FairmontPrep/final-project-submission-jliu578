03dee2b01b37550a4a59a5716cf8d7ad779baf07
jliu578 <jliu578@students.fairmontschools.com>
2025-05-30 09:09:04 -0700

    Updated

--- PathFinding.java
+++ PathFinding.java
@@ -25,63 +25,50 @@ public class PathFinding {
     static boolean[][] visited1 = new boolean[A.length][A[0].length];
     static boolean[][] visited2 = new boolean[B.length][B[0].length];
 
-    private static void dfs(int[][] matrix, boolean[][] visited, int row, int col) {
-        if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length) return;
-        if (matrix[row][col] != 1 || visited[row][col]) return;
+    private static boolean findPath(int[][] matrix, boolean[][] visited, int row, int col, int endRow, int endCol, List<String> path) {
+        if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length) return false;
+        if (matrix[row][col] != 1 || visited[row][col]) return false;
 
         visited[row][col] = true;
-        answerList.add("[" + row + "][" + col + "]");
+        path.add("[" + row + "][" + col + "]");
 
-        dfs(matrix, visited, row - 1, col);
-        dfs(matrix, visited, row + 1, col);
-        dfs(matrix, visited, row, col - 1);
-        dfs(matrix, visited, row, col + 1);
+        if (row == endRow && col == endCol) {
+            return true;
+        }
+
+        if (findPath(matrix, visited, row - 1, col, endRow, endCol, path) ||
+            findPath(matrix, visited, row + 1, col, endRow, endCol, path) ||
+            findPath(matrix, visited, row, col - 1, endRow, endCol, path) ||
+            findPath(matrix, visited, row, col + 1, endRow, endCol, path)) {
+            return true;
+        }
+
+        path.remove(path.size() - 1);
+        return false;
     }
 
     public static void main(String[] args) {
+        int startRow = 0, startCol = 5;
+        int endRow = 6, endCol = 10;
 
-        System.out.println("A");
-        int componentCount = 1;
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < A[0].length; j++) {
-                if (A[i][j] == 1 && !visited1[i][j]) {
-                    dfs(A, visited1, i, j);
-                    System.out.println("Component " + componentCount++ + ":");
-                    for (int x = 0; x < visited1.length; x++) {
-                        for (int y = 0; y < visited1[0].length; y++) {
-                            if (visited1[x][y]) {
-                                System.out.print("X ");
-                            } else {
-                                System.out.print(A[x][y] + " ");
-                            }
-                        }
-                        System.out.println();
-                    }
-                    answerList.clear();
-                }
+        System.out.println("Path from A:");
+        if (findPath(A, visited1, startRow, startCol, endRow, endCol, answerList)) {
+            for (String step : answerList) {
+                System.out.println(step);
             }
+        } else {
+            System.out.println("No path found.");
         }
 
-        System.out.println("B");
-        componentCount = 1;
-        for (int i = 0; i < B.length; i++) {
-            for (int j = 0; j < B[0].length; j++) {
-                if (B[i][j] == 1 && !visited2[i][j]) {
-                    dfs(B, visited2, i, j);
-                    System.out.println("Component " + componentCount++ + ":");
-                    for (int x = 0; x < visited2.length; x++) {
-                        for (int y = 0; y < visited2[0].length; y++) {
-                            if (visited2[x][y]) {
-                                System.out.print("X ");
-                            } else {
-                                System.out.print(B[x][y] + " ");
-                            }
-                        }
-                        System.out.println();
-                    }
-                    answerList.clear();
-                }
+        startRow = 0; startCol = 9;
+        endRow = 6; endCol = 0;
+        System.out.println("Path from B:");
+        if (findPath(B, visited2, startRow, startCol, endRow, endCol, answerList)) {
+            for (String step : answerList) {
+                System.out.println(step);
             }
+        } else {
+            System.out.println("No path found.");
         }
     }
 }
